0e1d7a4e3aad10b74ce2941db642d168523e4083
Marcelo-Honorio <marhonorio@gmail.com>
2026-03-10 12:23:33 -0300

    extração de tabelas com camelot

--- laudo/builder.py
+++ laudo/builder.py
@@ -3,20 +3,54 @@ from laudo.formatters import fmt_moeda, fmt_percentual
 def montar_contexto_laudo(df, dados_input, resultados_pericia, caminhos_imagens=None):
     caminhos_imagens = caminhos_imagens or {}
 
-    contexto = {
-        "autor": dados_input.get("autor", ""),
-        "contrato": dados_input.get("contrato", ""),
-        "finalidade_op": dados_input.get("finalidade_op", ""),
-        "periodo": dados_input.get("periodo", ""),
-        "taxa_juros": fmt_percentual(resultados_pericia.get("taxa_juros")),
-        "valor_liberado": fmt_moeda(resultados_pericia.get("valor_liberado")),
-        "saldo_calculado": fmt_moeda(resultados_pericia.get("saldo_calculado")),
-        "saldo_cobrado": fmt_moeda(resultados_pericia.get("saldo_cobrado")),
-        "diferenca": fmt_moeda(resultados_pericia.get("diferenca")),
-        "conclusao": resultados_pericia.get("conclusao", ""),
-        "grafico_saldo": caminhos_imagens.get("grafico_saldo"),
-    }
+    '''ex: dados_input
+    {'cliente': 'Marcelo Honorio',
+    'agente': 'do réu',
+    'contrato': '123456',
+    'periodo': 'mensal',
+    'estornos': ['Seguro Penhor', 'Seguro de Vida'],
+    'juros': '12',
+    'pasta': 'NovaPasta',
+    'valor_parcela': '12000',
+    'numero_parcela': '5',
+    'tx_equivalente': 'diaria',
+    'finalidade_op': 'Custeio'}
+    '''
     return contexto
 
+path_pdf = "C:\\Users\\marce\\Downloads\\PDF\\03-Ficha Grafica.pdf"
 
-montar_contexto_laudo()
+with open(path_pdf, "rb") as f:
+    reader = PyPDF2.PdfReader(f)
+
+    for page_idx in range(len(reader.pages)):
+        texto = reader.pages[page_idx].extract_text() or ""
+
+
+import camelot
+import pandas as pd
+
+tables = camelot.read_pdf(
+    path_pdf,
+    pages="all"
+)
+
+print(tables)
+
+df = tables[0].df
+print(df)
+
+tables = camelot.read_pdf(
+    path_pdf,
+    pages="1",
+    flavor="stream",
+    row_tol=10,
+    column_tol=10
+)
+df = tables[0].df
+print(df)
+
+# limpar colunas
+df = df.loc[:, df.columns != ""]
+df = df.loc[:, ~df.columns.duplicated()]
+
